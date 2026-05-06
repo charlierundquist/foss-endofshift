ec3c445d485efdb73ecda0817992fdb3796469e2
Charles Rundquist <charles.rundquist@fossswimschool.com>
2026-05-06 15:59:55 -0500

    starting to add notes functionality

--- static/main.js
+++ static/main.js
@@ -77,17 +77,122 @@ function getAllNotes(){
     req.onsuccess = (event) => {
         const cursor = event.target.result
         if(cursor){
-            console.log(cursor.key + ": " + cursor.value.familyname)
-           
+            console.log(cursor)
+
+            let newNoteDiv = document.createElement("p")
+            newNoteDiv.innerHTML = cursor.value.clipboardstring + " | " + cursor.value.notes
+            notesList.appendChild(newNoteDiv)
             cursor.continue()
         } else {
             console.log("no more entries")
         }
 
-        document.getElementById("notesList").innerHTML = notesList.outerHTML
+        document.getElementById("getAllNotesResult").innerHTML = notesList.outerHTML
     }
 
     req.onerror = (event) => {
         console.log("error")
     }
 }
+
+async function parseClipboardString() {
+
+            let text = await navigator.clipboard.readText()
+            let split = text.split("___")
+
+            const freestyleLink = split[1]
+            const familyName = split[2]
+            const phoneNumber = split[3]
+            const childrenList = split[4]
+
+            let newNoteParent = document.createElement("div")
+            let infoCol = document.createElement("div")
+
+            let linkRow = document.createElement("p")
+            let linkDiv = document.createElement("a")
+            linkDiv.setAttribute("href", freestyleLink)
+            linkDiv.setAttribute("target", "_blank")
+            linkDiv.innerHTML = "View in Freestyle"
+            linkRow.appendChild(linkDiv)
+            infoCol.appendChild(linkRow)
+
+
+            let nameRow = document.createElement("p")
+            let nameDiv = document.createElement("span")
+            nameDiv.innerHTML = familyName
+            nameRow.appendChild(nameDiv)
+            infoCol.appendChild(nameRow)
+
+
+            let phoneRow = document.createElement("p")
+            let phoneDiv = document.createElement("span")
+            phoneDiv.innerHTML = phoneNumber
+            phoneRow.appendChild(phoneDiv)
+            infoCol.appendChild(phoneRow)
+
+
+            let childrenListSplit = childrenList.split(":::")
+            console.log(childrenListSplit)
+            childrenListSplit.map((child, i) => {
+                console.log(child)
+
+                if(child === "") return
+
+                let newChildParent = document.createElement("div")
+                newChildParent.style.marginLeft = "1rem"
+
+                let classSplit = child.split("<CLASS>")
+                let cName = classSplit[0]
+
+                let cNameRow = document.createElement("p")
+                let cNameDiv = document.createElement("span")
+                cNameDiv.innerHTML = cName
+                cNameRow.appendChild(cNameDiv)
+                newChildParent.appendChild(cNameRow)
+
+                if (classSplit.length > 1){
+                    let classInfo = classSplit[1].split("/")
+
+                    let classLevel = classInfo[1]
+                    let classDay = classInfo[2]
+                    let classTime = classInfo[3]
+    
+                    let classRow = document.createElement("p")
+    
+                    let levelDiv = document.createElement("span")
+                    levelDiv.innerHTML = classLevel + " | "
+                    classRow.appendChild(levelDiv)
+    
+                    let dayDiv = document.createElement("span")
+                    dayDiv.innerHTML = classDay + " | "
+                    classRow.appendChild(dayDiv)
+    
+                    let timeDiv = document.createElement("span")
+                    timeDiv.innerHTML = classTime
+                    classRow.appendChild(timeDiv)
+
+                    newChildParent.appendChild(classRow)
+
+                }
+
+                infoCol.appendChild(newChildParent)
+            })
+
+            let notesList = document.getElementById("notesList")
+            newNoteParent.appendChild(infoCol)
+
+            let notesCol = document.createElement("div")
+            let noteBox = document.createElement("textarea")
+            let submitNoteButton = document.createElement("button")
+            submitNoteButton.innerHTML = "Add Note"
+            submitNoteButton.onclick = () => {
+                notesCol.innerHTML = noteBox.value
+                addNote(text, noteBox.value)
+            }
+            notesCol.appendChild(noteBox)
+            notesCol.appendChild(submitNoteButton)
+            newNoteParent.appendChild(notesCol)
+
+            notesList.appendChild(newNoteParent)
+
+        }
